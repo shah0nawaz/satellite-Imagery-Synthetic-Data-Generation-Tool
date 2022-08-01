8fc5ac8db2d76b2c0f1396de4b53ad820b3df5a3
shah0nawaz <38689408+shah0nawaz@users.noreply.github.com>
2022-08-01 14:47:42 +0500

    A very useful tool for fast synthetic data generation in satellite imagery

---
+++ past_model.py
@@ -0,0 +1,98 @@
+# import the necessary packages
+import argparse
+import cv2
+import numpy as np
+from PIL import Image
+import os
+import glob
+import time
+
+class PatchAir():
+
+    def __init__(self, model_path, save_data_path):
+        #self.input_img = input_img
+        self.refPtx = []
+        self.refPty = []
+        self.model_name = model_path
+        self.data_folder = save_data_path + '*'
+
+    def click_and_crop(self, event, x, y, flags, param):
+        # grab references to the global variables
+        global refPtx, refPty, cropping
+
+        # if the left mouse button was clicked, record the starting
+        # (x, y) coordinates and indicate that cropping is being
+        # performed
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.refPtx.append(x)
+            self.refPty.append(y)
+
+    def main(self):
+        
+        # image = cv2.imread('2.png')
+        # MIragesize = Lergth=14.36m, Width=9.13m
+        # RafaleSize = W=10.30m   L=15.30m
+        # Su30MKI Length=21.9 WIdth=14.7m
+        cv2.namedWindow("image")
+        cv2.setMouseCallback("image", self.click_and_crop)
+        img = Image.open(self.model_name).convert('RGBA')
+        size = int(8 * (1 / 0.25)),int(12* (1 / 0.25))
+        #size = int(14.36 * (1 / 0.25)),int(9.13* (1 / 0.25))
+        img = img.rotate(angle=180, expand=1).resize(size)
+
+        for file in glob.glob(self.data_folder):
+
+            width, height = img.size
+            background = Image.open(file)
+            image = cv2.imread(file)
+            path, name = os.path.split(file)
+            print(path, name)
+
+            # keep looping until the 'q' key is pressed
+            while True:
+                # display the image and wait for a keypress
+                cv2.imshow("image", image)
+                key = cv2.waitKey(1) & 0xFF
+
+                # if the 'r' key is pressed, reset the cropping region
+                if key == ord("r"):
+                    image = clone.copy()
+
+                # if the 'c' key is pressed, break from the loop
+                elif key == ord("c"):
+                    # img = Image.open('1.jpg')
+                    # width,height = img.size
+                    # background = Image.open('2.png')
+                    # print(refPt)
+
+                    # background.paste(img, (refPt[0], refPt[1], refPt[0] + width, refPt[1]+height ))
+                    # background.save('out.png')
+
+                    break
+
+            # if there are two reference points, then crop the region of interest
+            # from teh image and display it
+
+            # close all open windows
+            print(self.refPtx)
+            print(self.refPty)
+            # for i in range(len(refPt)):
+
+            # 	print(refPt[])
+
+            for x, y in zip(self.refPtx, self.refPty):
+                #print(img.shape)
+                background.paste(img, (x, y), img)
+
+            #background.show('image')
+            time.sleep(2)
+
+            background.save('./results' + '/' + name)
+            image_re = cv2.imread(file)
+            cv2.imshow('image',image_re)
+            cv2.waitKey(500)
+            self.refPty = []
+            self.refPtx = []
+
+        cv2.destroyAllWindows()
+
---
+++ synthetic_data_generation.py
@@ -0,0 +1,107 @@
+from PIL import Image
+import os
+import cv2
+import numpy as np
+from past_model import PatchAir
+# Useful Constants and Variables
+cropping_list = []
+DATASET_DIR = './satellite_full/' # Folder 1
+# DATASET_DIR = './SIDS_India_New_2nd_Half/' # Folder 2
+
+IMG_EXTENSION = '.jpg'
+
+IMG_NAME = 'test.jpg'
+infile = os.path.join(DATASET_DIR, IMG_NAME)
+CHOP_SIZE = 608
+Image.MAX_IMAGE_PIXELS = None
+
+
+def click_func(event, x, y, flags, param):
+    """
+    Click function that fire-up against the user click event
+    :param event:
+    :param x:
+    :param y:
+    :param flags:
+    :param param:
+    :return:
+    """
+    global cropping_list
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x1 = x - CHOP_SIZE // 2
+        x2 = x + CHOP_SIZE // 2
+        y1 = y - CHOP_SIZE // 2
+        y2 = y + CHOP_SIZE // 2
+
+        cv2.rectangle(param, (x1, y1), (x2, y2), (0, 255, 0), thickness=3)
+        cv2.imshow(IMG_NAME, param)
+        cropping_list.append([x1, y1, x2, y2])
+
+
+
+def crop_patch(image, cr_item):
+    """
+    Function to crop the patch from the loaded image
+    :param image: Pillow image
+    :param cr_item: Box coordinates
+    :return:
+    """
+    width, height = image.size
+    box = (cr_item[0], cr_item[1],
+           cr_item[0] + CHOP_SIZE if cr_item[0] + CHOP_SIZE < width else width - 1,
+           cr_item[1] + CHOP_SIZE if cr_item[1] + CHOP_SIZE < height else height - 1)
+    # try:
+    fname = 'slice_%s_x%03d_y%03d.jpg' % (IMG_NAME.replace(IMG_EXTENSION, ''), cr_item[0], cr_item[1])
+    new_name = os.path.sep.join([DATASET_DIR, fname])
+    print(new_name)
+    image.crop(box).save('./crops/'+ fname)
+    #return image.crop(box)
+
+def show_loading():
+    """
+    Display the loadingIMG_NAMEas a feedback for the user
+    :return:
+    """
+    img_progress = np.ones(shape=(64, 64, 1))
+    cv2.putText(img_progress, 'WORKING ON IMAGE, PLEASE WEIGHT',
+                (0, 32),
+                cv2.FONT_HERSHEY_SIMPLEX, 1,
+                (255, 255, 255,), 1, cv2.LINE_AA, False)
+    cv2.imshow('Loading', img_progress)
+
+
+def main(model_path, save_data):
+    """
+    Main function to load the image and process it using the loop
+    :return:
+    """
+    img = cv2.imread(infile)
+    height, width, _ = img.shape
+    cv2.namedWindow(IMG_NAME, cv2.WINDOW_FREERATIO)
+    cv2.setMouseCallback(IMG_NAME, click_func, param=img)
+    patchair = PatchAir(model_path, save_data)
+    while True:
+        cv2.imshow(IMG_NAME, img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('e'):
+            show_loading()
+            img_clone = Image.open(infile)
+            for crop_item in cropping_list:
+                crop_patch(img_clone, crop_item)
+            #patchair.main()
+            img_clone.close()
+            cropping_list.clear()
+            break  # or load next image
+
+        elif key == ord('q'):
+            break
+    cv2.destroyAllWindows()
+    patchair.main()
+
+
+
+
+if __name__ == '__main__':
+    model_path = './models/car_white.png'
+    save_data = './crops/'
+    main(model_path, save_data)
